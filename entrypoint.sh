ad1a4b0db09475ed0cd195fa65d8d6fe81353ebd
Hendrik Brombeer <hendrikbrombeer@gmail.com>
2025-01-12 22:36:45 +0100

    bunny env

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,13 +8,6 @@ MOTD="§6Bunny Magic Containers - $REGION"
 
 sed -i '36s|^motd=.*|motd='"$MOTD"'|' /minecraft/server.properties
 
-echo "Bunny Variables:"
-echo "APPID: ${APPID:-Not Set}"
-echo "PODID: ${PODID:-Not Set}"
-echo "REGION: ${REGION}"
-echo "PUBLIC_ENDPOINTS: ${PUBLIC_ENDPOINTS:-Not Set}"
-echo "PODIP: ${PODIP:-Not Set}"
-echo "HOSTIP: ${HOSTIP:-Not Set}"
-echo "ZONE: ${ZONE:-Not Set}"
+env
 
 exec java -Xmx1G -Xms1G -jar paper.jar nogui
